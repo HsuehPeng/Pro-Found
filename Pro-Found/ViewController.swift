424dfed48abde99ce71fb351e6f45b2b91638b52
Hsueh Peng Tseng <qaz5899971@gmail.com>
2022-06-10 17:02:47 +0800

    Setup First commit

---
+++ Pro-Found/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pro-Found
+//
+//  Created by Hsueh Peng Tseng on 2022/6/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
