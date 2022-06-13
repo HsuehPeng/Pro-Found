7a0a67d4060366d5ae24e356f27a0c4dda15e9ab
Hsueh Peng Tseng <qaz5899971@gmail.com>
2022-06-13 20:35:46 +0800

    Add image view on profile page

--- Pro-Found/Controllers/ProfileViewController.swift
+++ Pro-Found/Controllers/ProfileViewController.swift
@@ -11,14 +11,29 @@ class ProfileViewController: UIViewController {
 
 	// MARK: - Properties
 	
+	private let imageView: UIImageView = {
+		let iv = UIImageView()
+		iv.contentMode = .scaleAspectFill
+		iv.backgroundColor = .red
+		iv.translatesAutoresizingMaskIntoConstraints = false
+		return iv
+	}()
+	
 	// MARK: - Lifecycle
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		view.backgroundColor = .white
+		print(view.frame.height)
+		setupUI()
 	}
 	
 	// MARK: - UI
+	
+	func setupUI() {
+		view.addSubview(imageView)
+		imageView.anchor(top: view.topAnchor, left: view.leftAnchor, right: view.rightAnchor, height: 812 * (280 / 812))
+	}
 	
 	// MARK: - Actions
 	
